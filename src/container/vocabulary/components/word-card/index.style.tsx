ee5a92f3667da9c4cce121b615a1d9ffec948242
qosquo <rr.zagitov.02@gmail.com>
2024-12-03 22:12:30 +0300

    patch: style WordCard component

--- src/container/vocabulary/components/word-card/index.style.tsx
+++ src/container/vocabulary/components/word-card/index.style.tsx
@@ -1,15 +1,25 @@
 import styled from "@emotion/styled";
 
 export const WordCardStyled = styled.button`
+	--bg-color: rgb(255, 255, 255);
+
   display: flex;
   text-align: start;
   flex-direction: column;
-  background-color: rgb(255, 255, 255);
+  background-color: var(--bg-color);
   width: 550px;
   padding: 20px;
   margin: 5px;
-  border-radius: 15px;
+	border: solid var(--bg-color);
+	border-radius: 15px;
   filter: drop-shadow(5px 5px 5px rgba(180, 180, 180, 0.4));
+	
+	&:hover {
+		--bg-color: rgb(127, 127, 127);
+	}
+
+	&:active {
+		--bg-color: rgb(63, 63, 63);
 `;
 
 export const WordCardTitle = styled.h3`
