02e87b30fde2ec8435bfcf563805d8acc8cd75b2
Ruslan Zagitov <52854515+qosquo@users.noreply.github.com>
2024-12-14 11:52:42 +0000

    patch: Remake Card component using "emotion" library (#2)
    * Remake Card component to use Emotion CSS-IN-JS
    
    * Delete node_modules from repository
    
    * Add hover effect to Card component

--- src/container/home/index.tsx
+++ src/container/home/index.tsx
@@ -1,6 +1,6 @@
 import './index.css';
 import React from 'react';
-import Card from './components/card';
+import Card from '../../components/card';
 import { getNavigationsValue } from '@brojs/cli';
 
 const dictionaries = [
