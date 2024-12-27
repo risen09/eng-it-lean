fcab8d69657569af12b8305705e0223ec88b7e8b
Oleg8520 <olegopokin@gmail.com>
2024-12-28 00:53:06 +0300

    feat: add Registration page

--- src/container/main/router.tsx
+++ src/container/main/router.tsx
@@ -5,6 +5,10 @@ import { getNavigationsValue } from "@brojs/cli";
 import Root from "./components/root";
 import HomePage from "../home";
 import DictionaryPage from "../dictionary";
+import MessagePage from "../message";
+import LoginPage from "../entry";
+import RegistrationPage from "../registration";
+import PersonalAccountPage from "../account";
 
 export const router = createBrowserRouter([
   {
@@ -19,6 +23,10 @@ export const router = createBrowserRouter([
         path: getNavigationsValue("eng-it-lean.dictionary"),
         element: <DictionaryPage />,
       },
+      {
+        path: getNavigationsValue("eng-it-lean.registration"),
+        element: <RegistrationPage />,
+      }
     ],
   },
 ]);
