d9b1dd9ef30a28bc9ee79a57b1a39e44aa896952
Ruslan Zagitov <rr.zagitov.02@gmail.com>
2025-01-25 16:34:14 +0300

    feat: add create card component to add new word
    This card is placed at the end of the list and supposed to add new words to the dictionary. Must be seen only for previleged users.

--- src/container/dictionary/index.tsx
+++ src/container/dictionary/index.tsx
@@ -1,5 +1,23 @@
 import React, { useState } from 'react';
-import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import {
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBIcon,
+  MDBModal,
+  MDBModalDialog,
+  MDBModalHeader,
+  MDBModalTitle,
+  MDBBtn,
+  MDBModalContent,
+  MDBModalBody,
+  MDBInput,
+  MDBTextArea,
+  MDBListGroup,
+  MDBListGroupItem
+} from 'mdb-react-ui-kit';
 import WordItem from './components/WordItem';
 import { Word } from './types';
 import { useParams } from 'react-router-dom';
@@ -9,19 +27,65 @@ const DictionaryPage = (): React.ReactElement => {
   const { id } = useParams();
   const { data: dictionary, isLoading, error } = useGetDictionaryWordsQuery(parseInt(id));
 
+  const [isCreateModalOpened, setIsCreateModalOpened] = useState(false);
+
   return (
-    <MDBRow>
-      {error && <MDBCol md="12">Что-то пошло не так</MDBCol>}
+    <>
+      <MDBRow>
+        {error && <MDBCol md="12">Что-то пошло не так</MDBCol>}
 
-      {isLoading && <MDBCol md="12">Загрузка...</MDBCol>}
+        {isLoading && <MDBCol md="12">Загрузка...</MDBCol>}
 
-      {dictionary &&
-        dictionary.words?.map((word: Word) => (
-          <MDBCol xl={4} lg={6} className="mb-4">
-            <WordItem key={word.id} word={word} />
-          </MDBCol>
-        ))}
-    </MDBRow>
+        {dictionary &&
+          dictionary.words?.map((word: Word) => (
+            <MDBCol xl={4} lg={6} className="mb-4">
+              <WordItem key={word.id} word={word} />
+            </MDBCol>
+          ))}
+
+        <MDBCol xl={4} lg={6} className="mb-4">
+          <MDBCard tag="button" onClick={() => setIsCreateModalOpened(true)}>
+            <MDBCardBody className="h-70">
+              <MDBIcon fas icon="add" color="secondary" size="2xl" />
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+
+      <MDBModal tabIndex="-1" open={isCreateModalOpened} setOpen={setIsCreateModalOpened}>
+        <MDBModalDialog centered>
+          <MDBModalContent>
+            <MDBModalHeader>
+              <MDBModalTitle>Добавить термин</MDBModalTitle>
+              <MDBBtn className="btn-close" color="none" onClick={() => setIsCreateModalOpened(false)}></MDBBtn>
+            </MDBModalHeader>
+            <MDBModalBody>
+              <MDBListGroup>
+                <MDBListGroupItem noBorders>
+                  <MDBInput label="Термин" />
+                </MDBListGroupItem>
+                <MDBListGroupItem noBorders>
+                  <MDBTextArea label="Определение" />
+                </MDBListGroupItem>
+                <MDBListGroupItem noBorders>
+                  <MDBInput label="Синонимы">
+                    <div className="form-helper">Введите синонимы через запятую</div>
+                  </MDBInput>
+                </MDBListGroupItem>
+                <MDBListGroupItem noBorders>
+                  <MDBTextArea label="Примеры" />
+                </MDBListGroupItem>
+                <MDBListGroupItem noBorders>
+                  <MDBBtn color="primary" onClick={() => setIsCreateModalOpened(false)}>
+                    Добавить
+                  </MDBBtn>
+                </MDBListGroupItem>
+              </MDBListGroup>
+            </MDBModalBody>
+          </MDBModalContent>
+        </MDBModalDialog>
+      </MDBModal>
+    </>
   );
 };
 
