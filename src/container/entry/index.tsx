86b7047e963970d42b3a453721bcc99043013689
Oleg8520 <olegopokin@gmail.com>
2025-01-27 20:54:24 +0300

    Add Bootstrap

--- src/container/entry/index.tsx
+++ src/container/entry/index.tsx
@@ -1,34 +1,34 @@
-import './entry.css';
 import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdb-react-ui-kit';
 
 const LoginPage: React.FC = () => {
+  const { register, handleSubmit } = useForm();
+
   const handleCancel = (): void => {
-    //window.location.href = "file:///C:/Users/olego/Desktop/СберХаб/Сайт_eng2/Сайт_eng/app/templates/base.html";
+    
   };
 
-  const handleEntry = (): void => {
-    //window.location.href = "file:///C:/Users/olego/Desktop/СберХаб/Account/personal_account.html";
+  const handleEntry = (data): void => {
+    console.log(data);
   };
 
   return (
-    <form>
-      <div className="container">
-        <h1>Вход</h1>
-        <p>Заполните поля, расположенные ниже, чтобы войти в свой аккаунт.</p>
-        <hr />
+      <MDBRow>
+        <MDBCol>
+          <h1>Вход</h1>
+          <p>Заполните поля, расположенные ниже, чтобы войти в свой аккаунт.</p>
+          <hr />
 
-        <label htmlFor="email"><b>Электронная почта</b></label>
-        <input type="text" placeholder="Введите электронную почту" name="email" required />
+          <form onSubmit={handleSubmit(handleEntry)}>
+            <MDBInput label='Электронная почта' id='form1' type='email' required {...register('email')} />
+            <MDBInput label='Пароль' id='form2' type='password' required {...register('password')} />
 
-        <label htmlFor="password"><b>Пароль</b></label>
-        <input type="password" placeholder="Введите пароль" name="password" required />
-
-        <div className="clearfix">
-          <button type="button" className="cancelbtn" onClick={handleCancel}>Отменить</button>
-          <button type="button" className="entrybtn" onClick={handleEntry}>Войти</button>
-        </div>
-      </div>
-    </form>
+            <MDBBtn color='secondary' onClick={handleCancel}>Отменить</MDBBtn>
+            <MDBBtn color='primary' type='submit'>Войти</MDBBtn>
+          </form>
+        </MDBCol>
+      </MDBRow>
   );
 };
 
