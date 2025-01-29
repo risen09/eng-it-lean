7c8f3b84e83a87216ca25f875947ca66b50df231
Ruslan Zagitov <rr.zagitov.02@gmail.com>
2025-01-29 16:00:21 +0300

    feat: add code block in MarkdownStyled

--- src/container/page/index.tsx
+++ src/container/page/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { useChat, useCompletion } from 'ai/react';
+import { useCompletion } from 'ai/react';
 import { getConfigValue } from '@brojs/cli';
 import { MDBBtn, MDBCol, MDBInput, MDBRow, MDBTypography } from 'mdb-react-ui-kit';
 import MarkdownStyled from '../../components/markdown';
@@ -19,20 +19,24 @@ export default function Page() {
 
   return (
     <>
-      <MDBTypography tag="h2" variant="h2">Генерация нового урока</MDBTypography>
+      <MDBTypography tag="h2" variant="h2">
+        Генерация нового урока
+      </MDBTypography>
       <MDBRow tag="form" onSubmit={handleSubmit} className="g-3 align-items-center">
         <MDBCol size={2} />
         <MDBCol size={6}>
-          <MDBInput value={input} name="prompt" onChange={handleInputChange} id='input' label="Введите тему урока" />
+          <MDBInput value={input} name="prompt" onChange={handleInputChange} id="input" label="Введите тему урока" />
         </MDBCol>
         <MDBCol size={2}>
-          <MDBBtn type='submit'>
-            Сгенерировать
-          </MDBBtn>
+          <MDBBtn type="submit">Сгенерировать</MDBBtn>
         </MDBCol>
         <MDBCol size={2} />
       </MDBRow>
-      <MarkdownStyled>{completion}</MarkdownStyled>
+      <MDBRow>
+        <MDBCol>
+          <MarkdownStyled>{completion}</MarkdownStyled>
+        </MDBCol>
+      </MDBRow>
     </>
   );
 }
