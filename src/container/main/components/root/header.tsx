d704159449b9a483e8a0708f3c95341d9c8dd829
Ruslan Zagitov <rr.zagitov.02@gmail.com>
2025-01-31 16:04:49 +0300

    fix: change links in headers

--- src/container/main/components/root/header.tsx
+++ src/container/main/components/root/header.tsx
@@ -19,8 +19,8 @@ import {
 export const App = () => {
   const pagesV1 = [
     { id: 1, title: 'Главная', navigation: 'eng-it-lean.main' },
-    { id: 2, title: 'Практика', navigation: 'eng-it-lean.unit' },
-    { id: 3, title: 'Материалы', navigation: 'eng-it-lean.main' },
+    { id: 2, title: 'Практика', navigation: 'eng-it-lean.main' },
+    { id: 3, title: 'Материалы', navigation: 'eng-it-lean.units' },
     { id: 4, title: 'Прогресс', navigation: 'eng-it-lean.main' },
     { id: 5, title: 'Ссылки', navigation: 'eng-it-lean.main' }
   ];
