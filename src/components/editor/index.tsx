239a652ecad1947a063b99cda0a486d69edeef82
Ruslan Zagitov <rr.zagitov.02@gmail.com>
2025-02-01 23:11:58 +0300

    chore: extracted mdxeditor to common editor component with initialized settings

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,9 +1,67 @@
-import { MDXEditor } from '@mdxeditor/editor'
-import '@mdxeditor/editor/style.css'
-import React from 'react'
+import {
+  BlockTypeSelect,
+  BoldItalicUnderlineToggles,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  CodeToggle,
+  headingsPlugin,
+  InsertCodeBlock,
+  InsertTable,
+  listsPlugin,
+  ListsToggle,
+  markdownShortcutPlugin,
+  MDXEditor,
+  MDXEditorMethods,
+  MDXEditorProps,
+  quotePlugin,
+  tablePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin,
+  UndoRedo
+} from '@mdxeditor/editor';
+import '@mdxeditor/editor/style.css';
+import React, { ForwardedRef } from 'react';
 
-const Editor = ({ markdown } : { markdown: string }): React.ReactElement => {
-  return <MDXEditor markdown={markdown} />
-}
+const Editor = ({ editorRef, ...props }: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) => {
+  return (
+    <MDXEditor
+      ref={editorRef}
+      {...props}
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        tablePlugin(),
+        markdownShortcutPlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: 'js' }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            js: 'JavaScript',
+            css: 'CSS',
+            jsx: 'JavaScript (React)',
+            ts: 'TypeScript',
+            tsx: 'TypeScript (React)'
+          }
+        }),
+        toolbarPlugin({
+          toolbarClassName: 'my-classname',
+          toolbarContents: () => (
+            <>
+              {' '}
+              <UndoRedo />
+              <BlockTypeSelect />
+              <BoldItalicUnderlineToggles />
+              <CodeToggle />
+              <InsertCodeBlock />
+              <InsertTable />
+              <ListsToggle />
+            </>
+          )
+        })
+      ]}
+    />
+  );
+};
 
-export default Editor
+export default Editor;
