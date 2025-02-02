db53aff493a31097f1e3246f9281eeb559834046
Ruslan Zagitov <rr.zagitov.02@gmail.com>
2025-02-02 20:50:13 +0300

    chore: add banner to UnitsPage

--- src/container/units-list/index.tsx
+++ src/container/units-list/index.tsx
@@ -2,7 +2,9 @@ import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBCardTitle, MDBBtn, MDBTypograp
 import React from 'react';
 import { useGetUnitsQuery } from '../../store/api';
 import { LinkContainer } from 'react-router-bootstrap';
-import { getConfigValue, getNavigationValue } from '@brojs/cli';
+import { getConfigValue, getNavigationsValue, getNavigationValue } from '@brojs/cli';
+import { Link } from 'react-router-dom';
+import mainKurs from './images/bannerblock1.jpg';
 
 const UnitsPage = (): React.ReactElement => {
   const { data: units, isLoading, error } = useGetUnitsQuery();
@@ -10,12 +12,29 @@ const UnitsPage = (): React.ReactElement => {
   return (
     <>
       <div className="container my-2 py-5">
-        <MDBTypography tag="h1">Уроки</MDBTypography>
-        <MDBRow>
-          <MDBCol md="3" className='mb-4'>
-            <LinkContainer to={`${getNavigationValue('eng-it-lean.generate-unit')}`}>
-              <MDBBtn color="success">Сгенерировать урок по запросу</MDBBtn>
-            </LinkContainer>
+        <MDBTypography tag="h2" className="text-muted text-center">
+          Уроки
+        </MDBTypography>
+        <MDBRow className="align-items-center mb-5">
+          <MDBCol md="5" className="pe-md-5 border-end ms-5">
+            <MDBTypography tag="h2" className="fw-bold mb-5">
+              Начать обучение
+            </MDBTypography>
+            <MDBTypography tag="p" className="text-muted mb-5 fs-6">
+              Изучайте английский с нами. С нуля! <br />
+              Практики, словари, видеоуроки и многое другое <br />
+              Встроенный помощник
+            </MDBTypography>
+            <MDBBtn
+              tag={Link}
+              to={getNavigationsValue('eng-it-lean.generate-unit')}
+              color="success"
+            >
+              Сгенерировать урок
+            </MDBBtn>
+          </MDBCol>
+          <MDBCol md="6" className="text-center">
+          <img src={mainKurs} alt="Learning Illustration" className="img-fluid rounded" width="75%" height="75%" />
           </MDBCol>
         </MDBRow>
         <MDBRow>
@@ -28,7 +47,7 @@ const UnitsPage = (): React.ReactElement => {
               <MDBCol xl={4} lg={6} className="mb-4">
                 <MDBCard key={unit.id}>
                   <MDBCardBody>
-                    <MDBCardTitle>{unit.name}</MDBCardTitle>
+                    <MDBCardTitle className='text-muted '>{unit.name}</MDBCardTitle>
                     <LinkContainer to={`${getNavigationValue('eng-it-lean.unit').replace(':id', unit.id.toString())}`}>
                       <MDBBtn color="success">Изучить</MDBBtn>
                     </LinkContainer>
@@ -38,8 +57,8 @@ const UnitsPage = (): React.ReactElement => {
             ))}
         </MDBRow>
       </div>
-      </>
-      );
-      };
+    </>
+  );
+};
 
-      export default UnitsPage;
+export default UnitsPage;
